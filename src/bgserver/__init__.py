f1ad29b998c115c20e876c774253706982451edf
Ava Drumm <lmbdrumm@gmail.com>
2021-12-28 12:53:07 +0100

    Now works with proxy and CORS

--- src/bgserver/__init__.py
+++ src/bgserver/__init__.py
@@ -1,10 +1,18 @@
+"""The main flask app for the BullseyeGolf backend."""
+
 from flask import Flask
 from flask_restful import Api
-from .resources.routes import initialize_routes
+from .routes import initialize_routes
+from flask_cors import CORS
+from werkzeug.middleware.proxy_fix import ProxyFix
 
-app = Flask(__name__)
-api = Api(app)
+def create_app(test_config=None):  # Create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    CORS(app)
+    api = Api(app)
+    app.wsgi_app = ProxyFix(app.wsgi_app)
 
-initialize_routes(api)
+    initialize_routes(api)
 
-app.run()
+    return app
+
