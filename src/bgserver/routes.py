c03083cc9fa0d06c46ce5e1360af969d6aa1eec7
Ava Drumm <lmbdrumm@gmail.com>
2022-05-14 13:54:25 +0200

    Creates DB directory if it doesnt exist

--- src/bgserver/routes.py
+++ src/bgserver/routes.py
@@ -1,6 +1,7 @@
 from .resources import *
 from .dataclass import read
 from .generate_key import generate_key
+import os
 
 def initialize_routes(api):
     try:
@@ -9,6 +10,8 @@ def initialize_routes(api):
         generate_key()
         api_key = read('/bullseyegolf/credentials/admin_key')
         print(f'No API key was found, a new one was generated:\n{api_key}')
+
+    os.makedirs('/bullseyegolf/DB', exist_ok=True)
 
     api.add_resource(UserTournament, '/user/<db_id>')
     api.add_resource(Admin, f'/admin/{api_key}/<db_id>')
