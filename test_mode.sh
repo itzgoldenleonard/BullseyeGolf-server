5a49bb0cd803086abfc5a8b05a88b46852b25a05
Ava Drumm <lmbdrumm@gmail.com>
2022-05-15 14:16:18 +0200

    Clean slate for 2.0

--- test_mode.sh
+++
@@ -1,7 +0,0 @@
-mv Dockerfile Dockerfile.main
-mv Dockerfile.test Dockerfile
-
-mv docker-compose.yml docker-compose.yml.main
-mv docker-compose.yml.test docker-compose.yml
-
-echo "Entered testing mode, make sure to switch back before making any commits"
